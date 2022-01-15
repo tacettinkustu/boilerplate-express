62384e66226f601034a1a558a33b589fe5c56814
Tacettin Kustu <tacettinkustu@gmail.com>
2022-01-15 23:06:02 +0300

    Comment unnecessary partts

--- myApp.js
+++ myApp.js
@@ -2,18 +2,18 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
-app.use('/public', express.static(__dirname + '/public'));
+// app.use('/public', express.static(__dirname + '/public'));
 
-console.log('Hello World');
+// console.log('Hello World');
+
+// // app.get('/', (req, res) => {
+// //   res.send('Hello Express');
+// // });
 
 // app.get('/', (req, res) => {
-//   res.send('Hello Express');
+//   absolutePath = __dirname + '/views/index.html';
+//   return res.sendFile(absolutePath);
 // });
-
-app.get('/', (req, res) => {
-  absolutePath = __dirname + '/views/index.html';
-  return res.sendFile(absolutePath);
-});
 
 app.get('/json', (req, res) => {
   process.env.MESSAGE_STYLE === 'uppercase'
