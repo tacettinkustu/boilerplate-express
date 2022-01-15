0448cf4fc722f0661729cf3473c56a4781b1c856
Tacettin Kustu <tacettinkustu@gmail.com>
2022-01-15 20:47:13 +0300

    Send file as a response to get request

--- myApp.js
+++ myApp.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
+
 console.log('Hello World');
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Express');
+// });
+
 app.get('/', (req, res) => {
-  res.send('Hello Express');
+    res.sendFile('./views/index.html')
 });
 
 module.exports = app;
