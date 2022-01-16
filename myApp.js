0a8283811edd043dc6e41d151cb41e2118a36fd3
Tacettin Kustu <tacettinkustu@gmail.com>
2022-01-16 11:50:02 +0300

    Add function to the respond

--- myApp.js
+++ myApp.js
@@ -43,9 +43,8 @@ app.get(
   }
 );
 
-app.get(
-  '/:word/echo',
-  res.json({echo: req.params.word})
-)
+app.get('/:word/echo', function (req, res) {
+  res.json({ echo: req.params.word });
+});
 
 module.exports = app;
